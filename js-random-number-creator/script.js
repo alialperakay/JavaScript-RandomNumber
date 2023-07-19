15806df01aa877d5bee9a629c50a92e06d66d111
alialperakay <75796001+alialperakay@users.noreply.github.com>
2023-07-19 19:32:11 +0300

    Add files via upload

---
+++ js-random-number-creator/script.js
@@ -0,0 +1,11 @@
+
+
+
+function number(){
+    const random=(Math.random()*10);
+    document.getElementById("randomNumber").innerHTML=random;
+    document.getElementById("randomNumber").textContent=random;
+}
+
+
+
